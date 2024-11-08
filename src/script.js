c933cb0d0890db653ed9d056c7347f1763030fb1
Edilson Nogueira <edilsonnsf2005@gmail.com>
2024-11-08 20:35:05 -0300

    criado função de desabilitar links e features atualizadas

--- src/script.js
+++ src/script.js
@@ -2,15 +2,16 @@ const pergunta = document.getElementById('question');
 const botao = document.getElementById('next');
 const lista = document.getElementById('list');
 const resposta = document.getElementById('answer');
-const tentativa = document.getElementById('try');
 
 const questionario = [{
     pergunta: "1°) What is the smallest planet in our Solar System?", 
-    lista: ["Venus", "Mars", "Mercury", "Earth", "Saturn"]
+    lista: ["Venus", "Mars", "Mercury", "Earth", "Saturn"],
+    respostaCerta: "Mercury"
 }, 
 {
     pergunta: "2°) What is the longest river in the world?",
-    lista: ["Amazon", "Nile", "Yangtze", "Mississippi", "Níger"]
+    lista: ["Amazon", "Nile", "Yangtze", "Mississippi", "Níger"],
+    respostaCerta: "Nile"
 }]
 
 let perguntaAtual = 0;
@@ -19,59 +20,64 @@ const telaInicial = () => {
     let quest = questionario[perguntaAtual];
     pergunta.innerHTML = `<h2>${quest.pergunta}</h2>`
     lista.innerHTML = "";
-    for (let i = 0; i < quest.lista.length; i++) {
-        lista.innerHTML += `<li class="lista wrong"><a href="#">${quest.lista[i]}</a></li>`
-    }
-}
-/*
-pergunta.innerHTML = `
-    <h2>1°) What is the smallest planet in our Solar System?</h2>
-`*/
-/*lista.innerHTML = `
-    <li class="lista wrong"><a href="#">Venus</a></li>
-    <li class="lista wrong"><a href="#">Mars</a></li>
-    <li class="lista right"><a href="#">Mercury</a></li>
-    <li class="lista wrong"><a href="#">Earth</a></li>
-    <li class="lista wrong"><a href="#">Saturn</a></li>
-`;*/
 
-const listaItem = document.querySelectorAll('.lista');
+    quest.lista.forEach((opcao) => {
+        const item = document.createElement('li');
+        item.classList.add('lista')
+        item.innerHTML = `<a href="#">${opcao}</a>`;
 
-const arrayItem = Array.from(listaItem);
+        item.addEventListener('click', (event) => {
+            const clickItem = event.target;
+            const respostaClick = opcao;
+            desabilitarLinks();
+            wrongOrRight(respostaClick, clickItem, quest)
+        })
+        lista.appendChild(item);
+    });
+};
 
-const wrongOrRight = (item, clickItem) => {
-    if (item.classList.contains('wrong')) {
-        clickItem.style.backgroundColor = "red";
-        clickItem.style.color = "white";
-        resposta.innerHTML = `
-            <p style="color:red">You're wrong! Try again!</p>
-        `;
-    } else if (item.classList.contains('right')) {
+const desabilitarLinks = () => {
+    const allLinks = document.querySelectorAll('.lista a')
+    allLinks.forEach(link => {
+        if (link.classList.contains('disabled-link')) {
+            link.classList.remove('disabled-link');
+        } else {
+            link.classList.add('disabled-link');
+        }
+    });
+};
+
+const wrongOrRight = (respostaClick, clickItem, quest) => {
+    if (respostaClick === quest.respostaCerta) {
         clickItem.style.backgroundColor = "green";
         clickItem.style.color = "white";
         resposta.innerHTML = `
             <p style="color:green">You're right!</p>
         `;
+    } else {
+        clickItem.style.backgroundColor = "red";
+        clickItem.style.color = "white";
+        resposta.innerHTML = `
+            <p style="color:red">You're wrong! Try again!</p>
+        `;
+        botao.textContent = "Try Again!";
     }
 }
-//colocar index no forEach para procurar pelos items sem indexOf
-arrayItem.forEach((item, index) => {
-    item.addEventListener('click', (event) => {
-        const clickItem = event.target;
-        if (index === 0) {
-            wrongOrRight(item, clickItem)
-        } else if (index >= 1 && index <= 4){
-            wrongOrRight(item, clickItem)
-        }
-    })
-})
 
-/*botao.addEventListener('click', () => {
-    if(num = 0) {
-        //pergunta.innetHtml... 2
-    } else if(num = 1) {
-        //pergunta.innerHtml... 3
+botao.addEventListener('click', () => {
+    if (botao.textContent === "Next Question") {
+        if (perguntaAtual < questionario.length - 1) {
+            perguntaAtual++;
+            resposta.innerHTML = ""
+            telaInicial()
+        } else {
+            alert('Você completou o jogo')
+            botao.disabled = true;
+        };
+    } else if (botao.textContent === "Try Again!"){
+        desabilitarLinks();
+        //adicionar duas funcções (desabilitar e habilitar)
     }
-})*/
+});
 
 telaInicial()
